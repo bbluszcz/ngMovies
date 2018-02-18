431f67fa91c24f7e0b0fedf4fa17350604a827cb
Bartłomiej Bluszcz <bbluszcz@gmail.com>
2018-02-19 00:01:55 +0100

    delete of old prototype pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // directives
 import { DropdownDirective } from './dropdown.directive';
-import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     DropdownDirective,
-    FilterPipe,
     DropdownFilterPipe,
     InputFilterPipe
   ],
@@ -18,7 +16,6 @@ import { FilterPipe } from './filter.pipe';
   exports: [
     CommonModule,
     DropdownDirective,
-    FilterPipe,
      DropdownFilterPipe,
     InputFilterPipe
   ],
